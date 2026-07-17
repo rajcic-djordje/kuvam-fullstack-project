2cdd0c622fdfef6fa7a0b3ef1b30cc0aa83744c6
Djordje Rajcic <84-2022@pmf.kg.ac.rs>
2026-07-17 19:15:58 +0200

    App setup

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,10 @@
 import env from "./src/config/env.js"
 
-console.log("Kuvam backend starting on ${env.nodeEnv} mode.")
+import app from "./src/app.js"
+
+console.log(`Kuvam backend starting on ${env.node_env} mode on port ${env.node_env_port}.`)
+
+
+app.listen(env.node_env_port)
+
 
--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -1,12 +1,13 @@
 const node_env = process.env.NODE_ENV
-
+const node_env_port = process.env.PORT
 const allowed_values = ["development", "test", "production"]
 
 if (node_env==undefined || allowed_values.find(x => x == node_env) == undefined)
     throw new Error("Mode not allowed!")
 
 const env = {
-    node_env
+    node_env,
+    node_env_port
 }
 
 export default env
